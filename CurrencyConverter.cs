8f05cd1b980cb3e71d825452d1aabce472588fcc
anciii <24-07949@g.batstate-u.edu.ph>
2026-02-09 10:46:38 +0800

    Add files via upload

---
+++ CurrencyConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter amount in USD: ");
+        double usd = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double eur = usd * rate;
+
+        Console.WriteLine("Amount in EUR: {0:F2}", eur);
+    }
+}
